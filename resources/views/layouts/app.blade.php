8952682abc949b845bd1773119b9ec4a539f419f
jovialcore <jollydosky@gmail.com>
2021-07-19 11:53:28 +0100

    i finally did set the vuejs app running and kicking

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -15,59 +16,65 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+
+    <script defer src="{{ mix('js/app.js') }}"></script>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}" />
+
     <!-- Favicon -->
-		<link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/img/favicon.png') }}">
-        <script src="{{ asset('js/app.js') }}" defer></script>
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/img/favicon.png') }}">
 
-		<!-- Bootstrap CSS -->
-		<link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
 
-		<!-- Fontawesome CSS -->
-		<link rel="stylesheet" href="{{ asset('assets/plugins/fontawesome/css/fontawesome.min.css') }}">
-		<link rel="stylesheet" href="{{ asset('assets/plugins/fontawesome/css/all.min.css') }}">
+    <!-- Fontawesome CSS -->
+    <link rel="stylesheet" href="{{ asset('assets/plugins/fontawesome/css/fontawesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/plugins/fontawesome/css/all.min.css') }}">
 
-		<!-- calendar CSS -->
-		<link rel="stylesheet" href="{{ asset('assets/plugins/simple-calendar/simple-calendar.css') }}">
+    <!-- calendar CSS -->
+    <link rel="stylesheet" href="{{ asset('assets/plugins/simple-calendar/simple-calendar.css') }}">
 
-		<!-- Feathericon CSS -->
-		<link rel="stylesheet" href="{{ asset('assets/css/feather.css') }}">
+    <!-- Feathericon CSS -->
+    <link rel="stylesheet" href="{{ asset('assets/css/feather.css') }}">
 
-		<!-- Main CSS -->
-		<link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    <!-- Main CSS -->
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
 </head>
-<body >
-    <div class="main-wrapper" >
-        @include('snippets.nav')
 
-        <!-- Page Wrapper -->
-        <div class="page-wrapper" id="app">
-            <div class="content container-fluid" >
-                @yield('content')
+<body>
+
+        <div class="main-wrapper" >
+            @include('snippets.nav')
+            <!-- Page Wrapper -->
+            <div class="page-wrapper"  >
+                <div class="content container-fluid" id="app">
+                </example-component> </example-component>
+                    @yield('content')
+                </div>
             </div>
+            <!-- /Page Wrapper -->
+            @include('snippets.notifications')
         </div>
-        <!-- /Page Wrapper -->
 
-        @include('snippets.notifications')
-    </div>
+    <!-- jQuery -->
+    <script src="{{ asset('assets/js/jquery-3.6.0.min.js') }}"></script>
 
-		<!-- jQuery -->
-		<script src="{{ asset('assets/js/jquery-3.6.0.min.js') }}"></script>
+    <!-- Bootstrap Core JS -->
+    <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
 
-		<!-- Bootstrap Core JS -->
-		<script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
+    <!-- Slimscroll JS -->
+    <script src="{{ asset('assets/plugins/slimscroll/jquery.slimscroll.min.js') }}"></script>
 
-		<!-- Slimscroll JS -->
-		<script src="{{ asset('assets/plugins/slimscroll/jquery.slimscroll.min.js') }}"></script>
+    <!-- Chart JS -->
+    <script src="assets/plugins/apexchart/apexcharts.min.js"></script>
+    <script src="{{ asset('assets/plugins/apexchart/dsh-apaxcharts.js') }}"></script>
 
-		<!-- Chart JS -->
-		<script src="assets/plugins/apexchart/apexcharts.min.js"></script>
-		<script src="{{ asset('assets/plugins/apexchart/dsh-apaxcharts.js') }}"></script>
+    <!-- calendar Js -->
+    <script src="{{ asset('assets/plugins/simple-calendar/jquery.simple-calendar.js') }}"></script>
+    <script src="{{ asset('assets/js/calander.js') }}"></script>
 
-		<!-- calendar Js -->
-		<script src="{{ asset('assets/plugins/simple-calendar/jquery.simple-calendar.js') }}"></script>
-		<script src="{{ asset('assets/js/calander.js') }}"></script>
+    <!-- Custom JS -->
+    <script src="{{ asset('assets/js/script.js') }}"></script>
+</body>
 
-		<!-- Custom JS -->
-		<script  src="{{ asset('assets/js/script.js') }}"></script>
-	</body>
 </html>
--- resources/views/snippets/Email/composeMail.blade.php
+++ resources/views/snippets/Email/composeMail.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-gjjg
-</example-component> </example-component>
+<mail></mail>
 
 @endsection
