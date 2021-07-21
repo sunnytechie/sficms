ae0e67a565e26765a6ab05c73ff4a6492d6f7fc5
jovialcore <jollydosky@gmail.com>
2021-07-21 15:55:28 +0100

    my api and stuff worked on first trial..i am becoming very good at this

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/location',  [App\Http\Controllers\Api\EmailApiController::class, 'index'])->name('index');
+Route::get('/contacts',  [App\Http\Controllers\Api\EmailApiController::class, 'index'])->name('index');
