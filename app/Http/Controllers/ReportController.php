1b17527244035489dff6d089b12570595f6f3fb2
SunnyTechie <sunnyict001@gmail.com>
2021-07-24 20:16:29 -0700

    pull

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -3,10 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Session;
+use Carbon\Carbon;
+use App\Models\User;
+use App\Models\Profile;
+use App\Models\Attendance;
+use Illuminate\Support\Facades\Auth;
 
 class ReportController extends Controller
 {
-    public function index() {
+    public function index(Attendance $attendance) {
+        $profileId = $profile->id;
+        $profile = $profile->id;
+        $profileId = $profile->id;
+        $profileId = $profile->id;
+        $profileId = $profile->id;
+
         return view('reports.index');
     }
 }
