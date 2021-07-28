c17fa20e73aa46a213840284f058b26b1d6eb865
jovialcore <jollydosky@gmail.com>
2021-07-28 14:19:17 +0100

    made adding the form for saving emails

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -1,9 +1,16 @@
 <?php
 
 namespace App\Http\Controllers;
+
+use App\Models\Area;
 use App\Models\Contact;
+use App\Models\Country;
+use App\Models\Chapter;
+use App\Models\State;
+use PragmaRX\Countries\Package\Countries;
 
 use Illuminate\Http\Request;
+
 
 class EmailController extends Controller
 {
@@ -19,15 +26,77 @@ class EmailController extends Controller
 
     public function listEmails()
     {
-            $contacts = Contact::all();
+        $contacts = Contact::all();
 
         return view('email.list', compact('contacts'));
     }
 
     public function addContact()
     {
-            $contacts = Contact::all();
+        $countries = new countries();
+        $majorContries = $countries->all();
+        $states = $countries->where('name.common', 'Nigeria')
+            ->first()
+            ->hydrateStates()
+            ->states
+            ->sortBy('name');
+        $areas = Area::all();
+        $chapters = Chapter::all();
+        return view('email.addContact', compact('majorContries', 'states', 'areas', 'chapters'));
+    }
 
-        return view('email.addcontact', compact('contacts'));
+    public function add(Request $request)
+    {
+
+        $validation = request()->validate([
+            'FirstName' => 'required',
+            'LastName' => 'required',
+            'title' => 'required',
+            'email' => 'required|unique:contacts,email',
+            'country' => 'required',
+            'state' => 'required',
+            'area'  => 'required',
+            'chapter' => 'required',
+
+        ]);
+
+        $country_id = Country::where('name', $request->country)->get()->id();
+        dd($country_id);
+        $country = new Country();
+        $country->name = $request->country;
+        $country->save();
+
+        $state_id = State::where('name', $request->state)->get()->id();
+        $state = new State();
+        $state->name = $request->state;
+        $state->country_id = $country_id;
+        $state->save();
+
+        $area_id = Area::where('name', $request->state)->get()->id();
+        $area = new Area();
+        $area->name = $request->area;
+        $area->state_id = $state_id;
+        $area->countries_id = $country_id;
+        $area->save();
+
+        $chapter = new Chapter();
+        $chapter->name = $request->chapter;
+        $chapter->country_id = $country_id;
+        $chapter->state_id = $state_id;
+        $chapter->areas_id =  $area_id;
+        $chapter->save();
+
+        $contact = new Contact();
+        $country->name = $request->name;
+        $contact->title = $request->title;
+        $contact->email = $request->email;
+        $contact->chapters_id = $chapter->id;
+        $contact->states_id = $state->id;
+        $contact->areas_id = $area->id;
+        $contact->country_id = $country->id;
+        $contact->save();
+
+        return back()->with('msg', 'Successfull !!!');
+
     }
 }
--- resources/views/Email/addcontact.blade.php
+++ resources/views/Email/addcontact.blade.php
@@ -2,152 +2,116 @@
 
 @section('content')
 
-					<!-- Page Header -->
-					<div class="page-header">
-						<div class="row align-items-center">
-							<div class="col-md-12">
-								<div class="d-flex align-items-center">
-									<h5 class="page-title">Dashboard</h5>
-									<ul class="breadcrumb ml-2">
-										<li class="breadcrumb-item"><a href="index.html"><i class="fas fa-home"></i></a></li>
-										<li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
-										<li class="breadcrumb-item active">Horizontal Forms</li>
-									</ul>
-								</div>
-							</div>
-						</div>
-					</div>
-					<!-- /Page Header -->
+<!-- Page Header -->
+<div class="page-header">
+    <div class="row align-items-center">
+        <div class="col-md-12">
+            <div class="d-flex align-items-center">
+                <h5 class="page-title">Dashboard</h5>
+                <ul class="breadcrumb ml-2">
+                    <li class="breadcrumb-item"><a href="index.html"><i class="fas fa-home"></i></a></li>
+                    <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
+                    <li class="breadcrumb-item active">Horizontal Forms</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- /Page Header -->
 
+<div class="row">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-header">
+                <h5 class="card-title">Two Column Horizontal Form</h5>
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">Personal Information</h5>
+                <form action="/email/add" method="POST">
+                    @csrf
                     <div class="row">
-						<div class="col-md-12">
-							<div class="card">
-								<div class="card-header">
-									<h5 class="card-title">Two Column Horizontal Form</h5>
-								</div>
-								<div class="card-body">
-									<h5 class="card-title">Personal Information</h5>
-									<form action="#">
-										<div class="row">
-											<div class="col-xl-6">
-												<div class="form-group row">
-													<label class="col-lg-3 col-form-label">First Name</label>
-													<div class="col-lg-9">
-														<input type="text" class="form-control">
-													</div>
-												</div>
-												<div class="form-group row">
-													<label class="col-lg-3 col-form-label">Last Name</label>
-													<div class="col-lg-9">
-														<input type="text" class="form-control">
-													</div>
-												</div>
-												<div class="form-group row">
-													<label class="col-lg-3 col-form-label">Gender</label>
-													<div class="col-lg-9">
-														<div class="form-check form-check-inline">
-															<input class="form-check-input" type="radio" name="gender" id="gender_male" value="option1" checked>
-															<label class="form-check-label" for="gender_male">
-															Male
-															</label>
-														</div>
-														<div class="form-check form-check-inline">
-															<input class="form-check-input" type="radio" name="gender" id="gender_female" value="option2">
-															<label class="form-check-label" for="gender_female">
-															Female
-															</label>
-														</div>
-													</div>
-												</div>
-												<div class="form-group row">
-													<label class="col-lg-3 col-form-label">Blood Group</label>
-													<div class="col-lg-9">
-														<select class="select">
-															<option>Select</option>
-															<option value="1">A+</option>
-															<option value="2">O+</option>
-															<option value="3">B+</option>
-															<option value="4">AB+</option>
-														</select>
-													</div>
-												</div>
-											</div>
-											<div class="col-xl-6">
-												<div class="form-group row">
-													<label class="col-lg-3 col-form-label">Username</label>
-													<div class="col-lg-9">
-														<input type="text" class="form-control">
-													</div>
-												</div>
-												<div class="form-group row">
-													<label class="col-lg-3 col-form-label">Email</label>
-													<div class="col-lg-9">
-														<input type="text" class="form-control">
-													</div>
-												</div>
-												<div class="form-group row">
-													<label class="col-lg-3 col-form-label">Password</label>
-													<div class="col-lg-9">
-														<input type="password" class="form-control">
-													</div>
-												</div>
-												<div class="form-group row">
-													<label class="col-lg-3 col-form-label">Repeat Password</label>
-													<div class="col-lg-9">
-														<input type="password" class="form-control">
-													</div>
-												</div>
-											</div>
-										</div>
-										<h5 class="card-title">Address</h5>
-										<div class="row">
-											<div class="col-xl-6">
-												<div class="form-group row">
-													<label class="col-lg-3 col-form-label">Address Line 1</label>
-													<div class="col-lg-9">
-														<input type="text" class="form-control">
-													</div>
-												</div>
-												<div class="form-group row">
-													<label class="col-lg-3 col-form-label">Address Line 2</label>
-													<div class="col-lg-9">
-														<input type="text" class="form-control">
-													</div>
-												</div>
-												<div class="form-group row">
-													<label class="col-lg-3 col-form-label">State</label>
-													<div class="col-lg-9">
-														<input type="text" class="form-control">
-													</div>
-												</div>
-											</div>
-											<div class="col-xl-6">
-												<div class="form-group row">
-													<label class="col-lg-3 col-form-label">City</label>
-													<div class="col-lg-9">
-														<input type="text" class="form-control">
-													</div>
-												</div>
-												<div class="form-group row">
-													<label class="col-lg-3 col-form-label">Country</label>
-													<div class="col-lg-9">
-														<input type="text" class="form-control">
-													</div>
-												</div>
-												<div class="form-group row">
-													<label class="col-lg-3 col-form-label">Postal Code</label>
-													<div class="col-lg-9">
-														<input type="text" class="form-control">
-													</div>
-												</div>
-											</div>
-										</div>
-										<div class="text-right">
-											<button type="submit" class="btn btn-primary">Submit</button>
-										</div>
-									</form>
-								</div>
-							</div>
-						</div>
-					</div>
+                        <div class="col-xl-6">
+                            <div class="form-group row">
+                                <label class="col-lg-3 col-form-label">First Name</label>
+                                <div class="col-lg-9">
+                                    <input type="text" class="form-control" name="name">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-6">
+                            <div class="form-group row">
+                                <label class="col-lg-3 col-form-label">Email</label>
+                                <div class="col-lg-9">
+                                    <input type="Email" name="email" class="form-control">
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-lg-3 col-form-label">Title</label>
+                                <div class="col-lg-9">
+                                    <input type="text"  name="title" class="form-control" placeholder="Enter title...">
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                    <h5 class="card-title">Address</h5>
+                    <div class="row">
+                        <div class="col-xl-6">
+                            <div class="form-group row">
+                                <label class="col-lg-3 col-form-label">Country</label>
+                                <div class="col-lg-9">
+                                    <select class="select" name="country">
+                                        <option>Select</option>
+                                        @foreach ($majorContries as $country)
+                                        <option value="{{$country->name->common}}">{{$country->name->common}}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-lg-3 col-form-label">State</label>
+                                <div class="col-lg-9">
+                                    <select class="select" name="state">
+                                        @foreach ($states as $state)
+                                        <option>{{$state->name}}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-6">
+                            <div class="form-group row">
+                                <label class="col-lg-3 col-form-label">Area</label>
+                                <div class="col-lg-9">
+                                    <input list="areas" style="width: 100%; border:1px solid grey"
+                                        class="p-2 rounded-lg" name="area" />
+                                    <datalist id="areas">
+                                        @foreach($areas as $area)
+                                        <option>{{ $area->name }}</option>
+                                        @endforeach
+                                    </datalist>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-lg-3 col-form-label">Chapter</label>
+                                <div class="col-lg-9">
+                                    <input list="chapters" style="width: 100%; border:1px solid grey" class="p-2 rounded-lg "
+                                        name="chapter" />
+                                    <datalist id="chapters">
+                                        @foreach($chapters as $chapter)
+                                        <option>{{ $chapter->name }}</option>
+                                        @endforeach
+                                    </datalist>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="text-right">
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
