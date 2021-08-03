e2c037ebe4971517826121ca306a561c7e9eda79
jovialcore <jollydosky@gmail.com>
2021-08-03 21:00:50 +0100

    where there is a will, there is a way

--- app/Imports/EmailsIMport.php
+++ app/Imports/EmailsIMport.php
@@ -3,9 +3,12 @@
 namespace App\Imports;
 
 use App\Models\Contact;
+use App\Models\Country;
+use App\Models\State;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\ToModel;
+
 
 class EmailsIMport implements ToCollection
 {
@@ -17,15 +20,32 @@ class EmailsIMport implements ToCollection
         $y = $rows->toArray();
         $ro = array_splice($y, 1); // Remove headers
         foreach ($ro as $row) {
-            Contact::create([
-                'name' => $row[1],
-                // 'chapter' => $row[2],
-                // 'area' => $row[3],
-                // 'state' => $row[4],
-                // 'country' => $row[5],
-                'email' => $row[6],
-                'user_id' => $row[7],
-            ]);
+
+            // Country::create([
+            //     // 'name' => $row[1],
+            //     // 'chapter' => $row[2],
+            //     // 'area' => $row[3],
+            //     // 'state' => $row[4],
+            //     'name' => $row[5],
+            //     // 'email' => $row[6],
+            //     // 'user_id' => $row[7],
+            // ]);
+
+            $country = Country::where('name', $row[5])->updateOrCreate(['name' => $row[5]]);
+            $country_ids = $country->get('id');
+
+            foreach ($country_ids as $country_id) {
+                State::updateOrCreate([
+                    // 'name' => $row[1],
+                    // 'chapter' => $row[2],
+                    // 'area' => $row[3],
+                    'name' => $row[4],
+                    // 'name' => $row[5],
+                    // 'email' => $row[6],
+                    // 'user_id' => $row[7],
+                    'countries_id' => $country_id->id,
+                ]);
+            }
         }
     }
 }
