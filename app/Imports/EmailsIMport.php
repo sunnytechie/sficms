2defddebe09354e38cf8608e6873fad284273f9b
jovialcore <jollydosky@gmail.com>
2021-08-04 10:40:05 +0100

    I was able to save the coutries, states and areas with their respectives ids...so we are 80% done

--- app/Imports/EmailsIMport.php
+++ app/Imports/EmailsIMport.php
@@ -2,6 +2,7 @@
 
 namespace App\Imports;
 
+use App\Models\Area;
 use App\Models\Contact;
 use App\Models\Country;
 use App\Models\State;
@@ -19,32 +20,28 @@ class EmailsIMport implements ToCollection
     {
         $y = $rows->toArray();
         $ro = array_splice($y, 1); // Remove headers
+
         foreach ($ro as $row) {
-
-            // Country::create([
-            //     // 'name' => $row[1],
-            //     // 'chapter' => $row[2],
-            //     // 'area' => $row[3],
-            //     // 'state' => $row[4],
-            //     'name' => $row[5],
-            //     // 'email' => $row[6],
-            //     // 'user_id' => $row[7],
-            // ]);
-
+            //country saved
             $country = Country::where('name', $row[5])->updateOrCreate(['name' => $row[5]]);
             $country_ids = $country->get('id');
+            //state saved
+            State::updateOrCreate([
+                'name' => $row[4],
+            ]);
+            $size = count($country_ids); // it saves more memory to set the array length to a variable with the for loop
+            for ($i = 0; $i < $size; $i++) {
+                State::where('name', $row[4])->update(['countries_id' => $country_ids[$i]['id']]);
+            }
+            //area saved
+            $area = Area::updateOrCreate([
+                'name' => $row[3]
+            ]);
 
-            foreach ($country_ids as $country_id) {
-                State::updateOrCreate([
-                    // 'name' => $row[1],
-                    // 'chapter' => $row[2],
-                    // 'area' => $row[3],
-                    'name' => $row[4],
-                    // 'name' => $row[5],
-                    // 'email' => $row[6],
-                    // 'user_id' => $row[7],
-                    'countries_id' => $country_id->id,
-                ]);
+            $state_ids = State::get('id');
+            $stateArraySize = count($state_ids);
+            for ($i = 0; $i < $size; $i++) {
+                Area::where('name', $row[3])->update(['countries_id' => $country_ids[$i]['id'], 'state_id' => $state_ids[$i]['id']]);
             }
         }
     }
