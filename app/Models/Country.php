0b50f009e5a2a77d8587dc84f7ff9cda1ab5d630
jovialcore <jollydosky@gmail.com>
2021-08-10 14:21:44 +0100

    resetting the app well

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -9,7 +9,7 @@ class Country extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name'];
+    protected $fillable = ['name',  'title', 'email', 'country', 'state',  'area', 'chapter'];
 
     public  function contacts()
     {
