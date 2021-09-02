65771caece127a19ed5ecaed5511e7d9ea063a35
jovialcore <jollydosky@gmail.com>
2021-09-02 16:22:10 +0100

    I added the social media icons to the mail template footer

--- resources/views/vendor/mail/html/footer.blade.php
+++ resources/views/vendor/mail/html/footer.blade.php
@@ -1,11 +1,93 @@
 <tr>
-<td>
-<table class="footer" align="center" width="570" cellpadding="0" cellspacing="0" role="presentation">
-<tr>
-<td class="content-cell" align="center">
-{{ Illuminate\Mail\Markdown::parse($slot) }}
-</td>
+    <td>
+        <table align="center" border="0" cellpadding="0" cellspacing="0" class="row row-5" role="presentation"
+            style="mso-table-lspace:0;mso-table-rspace:0;background-color:#f7f6f5" width="100%">
+            <tbody>
+                <tr>
+                    <td>
+                        <table align="center" border="0" cellpadding="0" cellspacing="0" class="row-content stack"
+                            role="presentation" style="mso-table-lspace:0;mso-table-rspace:0;background-color:#072b52"
+                            width="570">
+                            <tbody>
+                                <tr>
+                                    <th class="column"
+                                        style="mso-table-lspace:0;mso-table-rspace:0;font-weight:400;text-align:left;vertical-align:top"
+                                        width="100%">
+                                        <table border="0" cellpadding="0" cellspacing="0" class="social_block"
+                                            role="presentation" style="mso-table-lspace:0;mso-table-rspace:0"
+                                            width="100%">
+                                            <tr>
+                                                <td
+                                                    style="text-align:center;padding-right:0;padding-left:0;padding-top:5px">
+                                                    <table align="center" border="0" cellpadding="0" cellspacing="0"
+                                                        class="social-table" role="presentation"
+                                                        style="mso-table-lspace:0;mso-table-rspace:0" width="168px">
+                                                        <tr>
+                                                            <td style="padding:0 5px 0 5px"><a
+                                                                    href="https://web.facebook.com/love4sfi"
+                                                                    target="_blank"><img alt="Facebook" height="32"
+                                                                        src="https://cdn.exclaimer.com/Handbook%20Images/facebook-icon_16x16.png"
+                                                                        style="display:block;height:auto;border:0"
+                                                                        title="Facebook" width="32" /></a>
+                                                            </td>
+                                                            <td style="padding:0 5px 0 5px"><a
+                                                                    href="https://twitter.com/love4sfi" target="_blank"><img
+                                                                        alt="Twitter" height="32"
+                                                                        src="https://cdn.exclaimer.com/Handbook%20Images/twitter-icon_16x16.png?_ga=2.116401222.1168369431.1630594523-392809839.1630594523"
+                                                                        style="display:block;height:auto;border:0"
+                                                                        title="Twitter" width="32" /></a>
+                                                            </td>
+                                                            <td style="padding:0 5px 0 5px"><a
+                                                                    href="https://www.instagram.com/sistersfellowshipinternational" target="_blank"><img
+                                                                        alt="Instagram" height="32"
+                                                                        src="https://cdn.exclaimer.com/Handbook%20Images/instagram-icon_16x16.png"
+                                                                        style="display:block;height:auto;border:0"
+                                                                        title="Instagram" width="32" /></a>
+                                                            </td>
+                                                            <td style="padding:0 5px 0 5px"><a
+                                                                    href="https://www.youtube.com/channel/UCeQpaXDTFJ12NJKjpIJAgyg" target="_blank"><img
+                                                                        alt="YouTube" height="32"
+                                                                        src="https://cdn.exclaimer.com/Handbook%20Images/youtube-icon_16x16.png"
+                                                                        style="display:block;height:auto;border:0"
+                                                                        title="YouTube" width="32" /></a>
+                                                            </td>
+                                                        </tr>
+                                                    </table>
+                                                </td>
+                                            </tr>
+                                        </table>
+                                        <table border="0" cellpadding="0" cellspacing="0" class="text_block"
+                                            role="presentation"
+                                            style="mso-table-lspace:0;mso-table-rspace:0;word-break:break-word"
+                                            width="100%">
+                                            <tr>
+                                                <td
+                                                    style="padding-bottom:15px;padding-left:10px;padding-right:10px;padding-top:10px">
+                                                    <div style="font-family:Tahoma,Verdana,sans-serif">
+                                                        <div
+                                                            style="font-size:12px;color:#f7f6f5;line-height:1.2;font-family:Lato,Tahoma,Verdana,Segoe,sans-serif">
+                                                            <p style="margin:0;text-align:center"><span
+                                                                    style="font-size:16px">Sisters
+                                                                    Fellowship International</span></p>
+                                                        </div>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        </table>
+                                    </th>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        {{-- <table class="footer" align="center" width="570" cellpadding="0" cellspacing="0" role="presentation">
+            <tr>
+                <td class="content-cell" align="center" >
+                    {{ Illuminate\Mail\Markdown::parse($slot) }}
+    </td>
 </tr>
-</table>
+</table> --}}
 </td>
 </tr>
