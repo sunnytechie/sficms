559c31a03f038ec9d4b91fec5c5af882e3ce0578
jovialcore <jollydosky@gmail.com>
2021-09-24 17:41:35 +0100

    done creating the importing feature for the forms that we will be filling

--- app/Http/Controllers/DatabankController.php
+++ app/Http/Controllers/DatabankController.php
@@ -4,9 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Imports\DatabankImport;
+use Maatwebsite\Excel\Facades\Excel;
+
 class DatabankController extends Controller
 {
-    public function import(){
+    public function import()
+    {
         return view('databank.import');
+    }
+
+
+    public  function importCSV(Request $request)
+    {
+        $file = $request->file('file');
+        Excel::import(new DatabankImport,  $file);
+        return back()->with('msg', 'Import Upload was successfull');
     }
 }
--- app/Models/Databank.php
+++ app/Models/Databank.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Databank extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['email',  'occupation', 'position', 'area', 'member',  'fullname', 'member', 'category', 'phone'];
+
 }
