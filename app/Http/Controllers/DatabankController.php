92aedd6eff8c11052bcdcf1f04c582a2b4827add
jovialcore <jollydosky@gmail.com>
2021-09-29 16:04:32 +0100

    I just added the exporting feature . Now you can also Import and export information depending on the category

--- app/Http/Controllers/DatabankController.php
+++ app/Http/Controllers/DatabankController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Exports\DatabankExport;
 use App\Imports\DatabankImport;
 use App\Models\Databank;
 use Maatwebsite\Excel\Facades\Excel;
@@ -18,10 +19,10 @@ class DatabankController extends Controller
         return view('databank.alldata', compact('categoryLists'));
     }
 
-    public function show($slug)
+    public function show($Catslug)
     {
-        $databankInfo = Databank::where('category', $slug)->get();
-        return view('databank.show', compact('databankInfo'));
+        $databankInfo = Databank::where('category', $Catslug)->get();
+        return view('databank.show', compact('databankInfo', 'Catslug'));
     }
 
     public function import()
@@ -34,6 +35,15 @@ class DatabankController extends Controller
     {
         $file = $request->file('file');
         Excel::import(new DatabankImport,  $file);
+
         return back()->with('msg', 'Import Upload was successfull');
+    }
+
+    public function export($catSlug)
+    {
+        //this is a little bit different fromt he normal excel export in that i am using the fromQuery query builder approach and not the default collection. I needed this because I not be export the whole databank but only the selected category from the db
+        //the $catSlug is the selected category aka, e.g, YDF, HOC, etc. You get ? catSlug=YDF in the url to export the YDF category only
+        //
+        return (new DatabankExport($catSlug))->download($catSlug.'Excel.xlsx'); // for e.g, YDFExcel.xlsx is the name of the file that will be exported
     }
 }
